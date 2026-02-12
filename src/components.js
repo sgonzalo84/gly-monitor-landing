3aee55d82cbed8205b1d73d9cb34c4b77a0c8a21
sgonzalo84 <sgonzalo84@gmail.com>
2026-02-12 19:33:37 +0100

    Actualizadas URLs con el dominio de glymo.es

--- src/components.js
+++ src/components.js
@@ -283,7 +283,7 @@ class GlymoLandingFooter extends HTMLElement {
               >
               <a
                 class="text-sm text-gray-600 dark:text-gray-300 hover:text-primary transition-colors font-medium"
-                href="https://gly-monitor.web.app/login"
+                href="https://app.glymo.es/login"
                 >Comenzar ahora</a
               >
             </div>
